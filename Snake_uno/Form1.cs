b5269773cc8da9b0b0404df421b7e203348f4595
pawel <pawel@PAWEL_PC>
2016-12-15 22:10:59 +0100

    8 commit; dodanie podstawowych komponentow gry

--- Snake_uno/Form1.cs
+++ Snake_uno/Form1.cs
@@ -12,9 +12,26 @@ namespace Snake_uno
 {
     public partial class Form1 : Form
     {
+
+        private List<punkt> Snake = new List<punkt>();
+        private punkt jedzenie = new punkt();
+
         public Form1()
         {
             InitializeComponent();
+
+            new glowne();
+
+            CzasGry.Interval = 3000 / glowne.Szybkosc;
+            CzasGry.Tick += AktualizacjaObrazu;
+            CzasGry.Start();
+
+            StartGame();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
