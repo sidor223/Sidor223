87a93cef4f321b98bdfb571a0cfc5826c2dcfb17
pawel <pawel@PAWEL_PC>
2016-12-15 21:50:07 +0100

    3 commit; dodanie parametrow klasy

--- Snake_uno/glowne.cs
+++ Snake_uno/glowne.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Snake_uno
 {
+
+    public enum Direction
+    {
+        wGore,
+        wDol,
+        wLewo,
+        wPrawo
+    };
+
     class glowne
     {
+        public static int Wysokosc { get; set; }
+        public static int Szerokosc { get; set; }
+        public static int Szybkosc { get; set; }
+        public static int Wynik { get; set; }
+        public static int Punkty { get; set; }
+        public static bool KoniecGry { get; set; }
+        public static Direction Kierunek { get; set; }
+
+        public glowne()
+
+        {
+            Szerokosc = 16;
+            Wysokosc = 16;
+            Szybkosc = 16;
+            Wynik = 0;
+            Punkty = 10;
+            KoniecGry = false;
+            Kierunek = Direction.wDol;
+
+        }
+
     }
 }
