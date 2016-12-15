10a3548f29ee98c20343c407f8af282e20566ce5
pawel <pawel@PAWEL_PC>
2016-12-15 22:05:33 +0100

    7 commit; dodanie kodu rozpoznajacego klawisze

--- Snake_uno/rozpoznawanie_klaiwszy.cs
+++ Snake_uno/rozpoznawanie_klaiwszy.cs
@@ -8,5 +8,21 @@ namespace Snake_uno
 {
     class rozpoznawanie_klaiwszy
     {
+        private static Hashtable keyTable = new Hashtable(); // zaladowanie listy dostepnych klawiszy do uzycia
+
+        public static bool KeyPressed(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool)keyTable[key];
+        }
+
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
     }
 }
